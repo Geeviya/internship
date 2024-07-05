1c671e98df530dcfa5c71a7cfed7f5d3f07dfff4
Geeviya <geeviya8989@gmail.com>
2024-07-05 10:03:21 +0530

    internship

---
+++ src/main/java/Basic/Createanarray.java
@@ -0,0 +1,9 @@
+package Basic;
+import java.util.Arrays;
+
+public class Createanarray {
+    public static void main(String[] args) {
+        String [] names={"Gv","Geevi"};
+        System.out.println(Arrays.toString(names));
+    }
+}
